29ef793e30d3e009b55b41fcfeba1667ba1f5fe8
José Antonio Pinto dos Santos <jose.antonio@noahsystems.com.br>
2023-11-07 08:26:37 -0400

    refactor: removed env.go file, it is not used anymore and be able to change variable name from error-code and error-message

--- ErrorCode.go
+++ ErrorCode.go
@@ -10,10 +10,12 @@ const (
 type getPropertyByName func(errorCode GenericErrorCode, propertyName string) string
 
 var GetPropertyByName getPropertyByName
+var ErrorCode = "error-code"
+var ErrorMessage = "error-message"
 
 func (g GenericErrorCode) GetCodeByEnv() string {
-	return GetPropertyByName(g, "error-code")
+	return GetPropertyByName(g, ErrorCode)
 }
 func (g GenericErrorCode) GetMessageByEnv() string {
-	return GetPropertyByName(g, "error-message")
+	return GetPropertyByName(g, ErrorMessage)
 }
